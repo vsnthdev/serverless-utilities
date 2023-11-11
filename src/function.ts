3ba8f43cab5287b64c7fea13d104b74f852b7961
Vasanth Srivatsa <vsnthdev@gmail.com>
2023-11-11 20:06:23 +0530

    ✨ add CORS + caching support

--- src/function.ts
+++ src/function.ts
@@ -8,6 +8,8 @@ import type { ZodObject } from 'zod'
 import { notFound } from './notFound.js'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
+export type RequestMethod = "get" | "head" | "post" | "put" | "delete" | "connect" | "options" | "trace" | "patch"
+
 export interface RouteConfig {
     handler: (req: VercelRequest, res: VercelResponse) => Promise<any>
     validation?: {
@@ -18,18 +20,27 @@ export interface RouteConfig {
 }
 
 export interface FunctionConfig {
-    get?: RouteConfig
-    put?: RouteConfig
-    head?: RouteConfig
-    post?: RouteConfig
-    trace?: RouteConfig
-    patch?: RouteConfig
-    delete?: RouteConfig
-    connect?: RouteConfig
-    options?: RouteConfig
+    methods: {
+        get?: RouteConfig
+        put?: RouteConfig
+        head?: RouteConfig
+        post?: RouteConfig
+        trace?: RouteConfig
+        patch?: RouteConfig
+        delete?: RouteConfig
+        connect?: RouteConfig
+        options?: RouteConfig
+    }
+    cors?: {
+        allowedOrigin?: string
+        allowedHeaders?: string[]
+        allowCredentials?: boolean
+        allowedMethods?: RequestMethod[]
+    }
+    caching?: {
+        sharedCacheSeconds?: number
+    }
 }
-
-export type RequestMethod = "get" | "head" | "post" | "put" | "delete" | "connect" | "options" | "trace" | "patch"
 
 async function validate(req: VercelRequest, res: VercelResponse, route: RouteConfig) {
     // validate route
@@ -77,9 +88,38 @@ export async function func(req: VercelRequest, res: VercelResponse, config: Func
     // get current request method
     const method = req.method.toLowerCase() as RequestMethod
 
+    if (config.cors?.allowCredentials) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+    }
+
+    if (config.cors?.allowedHeaders) {
+        res.setHeader('Access-Control-Allow-Headers', config.cors.allowedHeaders.join(', '))
+    }
+
+    if (config.cors.allowedMethods) {
+        res.setHeader('Access-Control-Allow-Methods', config.cors.allowedMethods.join(',').toUpperCase())
+    }
+
+    if (config.cors?.allowedOrigin) {
+        res.setHeader('Access-Control-Allow-Origin', config.cors.allowedOrigin)
+    }
+
+    // handle responding to OPTIONS request for CORS globally
+    if (method == 'options' && config.cors) {
+        return res.status(200).send('')
+    }
+
+    // handle caching
+    if (config.caching?.sharedCacheSeconds) {
+        res.setHeader(
+            'Cache-Control',
+            `max-age=0, s-maxage=${config.caching.sharedCacheSeconds}, stale-while-revalidate`,
+        )
+    }
+
     // grab the function that has been mapped to
     // the user sent request method
-    const route = config[method]
+    const route = config.methods[method]
 
     // handle when no method has been defined
     if (!route) return notFound(req, res)
